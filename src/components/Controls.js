6e538a63748e8774ca03888da425735fa8550f50
deepak-singh343 <deepak.negi343@gmail.com>
2022-12-21 15:05:40 +0530

    changed icons

--- src/components/Controls.js
+++ src/components/Controls.js
@@ -1,6 +1,11 @@
 import React from 'react'
 import { useState,useContext } from 'react';
 import { FaAngleLeft,FaAngleRight,FaSearchPlus,FaSearchMinus,FaPlay,FaPause,FaRulerHorizontal } from 'react-icons/fa';
+import {GrRotateLeft,GrRotateRight} from 'react-icons/gr'
+import {TfiZoomIn,TfiZoomOut} from 'react-icons/tfi'
+import {SlControlPlay,} from 'react-icons/sl'
+import {AiOutlinePause} from 'react-icons/ai'
+import {RxRulerSquare} from 'react-icons/rx'
 import {ThreeDServices} from '../services/threeDServices'
 import { AppContext } from "../context/AppContextProvider";
 const Controls = () => {
@@ -34,21 +39,21 @@ const Controls = () => {
   return (
     <div>
       <div className='w-full flex justify-center bottom-12 absolute'>
-        <div className='flex justify-around w-[20%]'>
+        <div className='flex justify-around w-[17%]'>
           <div className='rotate flex text-white cursor-pointer'>
-            <FaAngleLeft size={30} onClick={()=>handleModelRotationHorizontalRotation('left')}></FaAngleLeft>
-            <FaAngleRight size={30} onClick={()=>handleModelRotationHorizontalRotation('right')}></FaAngleRight>
+            <GrRotateRight size={30} onClick={()=>handleModelRotationHorizontalRotation('left')}/>
+            <GrRotateLeft size={30} onClick={()=>handleModelRotationHorizontalRotation('right')}/>
           </div>
           <div className='zoom flex text-white cursor-pointer'>
-            <FaSearchPlus size={30} onClick={()=>handleZoomInZoomOut('zoomIn')}></FaSearchPlus>
-            <FaSearchMinus size={30} onClick={()=>handleZoomInZoomOut('zoomOut')}></FaSearchMinus>
+            <TfiZoomIn size={30} onClick={()=>handleZoomInZoomOut('zoomIn')}/>
+            <TfiZoomOut size={30} onClick={()=>handleZoomInZoomOut('zoomOut')}/>
           </div>
           <div className='play-pause text-white cursor-pointer'>
-            {showPlayButton?(<FaPlay size={30} onClick={()=>handlePlayPauseButton('play')}></FaPlay>):''}
-            {showPauseButton?(<FaPause size={30} onClick={()=>handlePlayPauseButton('pause')}></FaPause>):''} 
+            {showPlayButton?(<SlControlPlay size={30} onClick={()=>handlePlayPauseButton('play')}/>):''}
+            {showPauseButton?(<AiOutlinePause size={30} onClick={()=>handlePlayPauseButton('pause')}/>):''} 
           </div>
           <div className='ruler  text-white cursor-pointer'>
-            <FaRulerHorizontal size={30} onClick={handleShowHideHotSpots}></FaRulerHorizontal>
+            <RxRulerSquare size={30} onClick={handleShowHideHotSpots}/>
           </div>
         </div>
     </div>
